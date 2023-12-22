3d52d165fa03ef924104fb7938d540923f4520d5
Ren Woodward <rwoodward1414@gmail.com>
2023-12-22 16:04:41 +1100

    Added week dates and buttons to scroll between weeks

--- client/src/Components/dashboard.jsx
+++ client/src/Components/dashboard.jsx
@@ -4,6 +4,9 @@ function Dashboard () {
 
     const navigate = useNavigate();
     const [projects, setProjects] = React.useState([]);
+    const [weekStart, setWeekStart] = React.useState(Date());
+    const daysOfWeek = ['Sun', 'Mon', 'Tues', 'Wed', 'Thur', 'Fri', 'Sat'];
+    const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
 
     React.useEffect(() => {
         const dashboard = async () => {
@@ -17,12 +20,15 @@ function Dashboard () {
             const data = await res.json();
             if (data === 'Invalid token'){
                 navigate('/login');
+                return null;
             } else {
-                setProjects(data);
+                return data;
             }
         };
-
-        dashboard();
+        const startDate = new Date();
+        startDate.setDate(startDate.getDate() - (startDate.getDay() - 1));
+        setWeekStart(startDate);
+        setProjects(dashboard());
     }, []);
 
     const Day = (d) => {
@@ -31,6 +37,41 @@ function Dashboard () {
                 <p>{d.d}</p>
                 <p>{d.date}</p>
             </div>
+        )
+    }
+
+    const back = () => {
+        const newWeekStart = new Date(weekStart);
+        newWeekStart.setDate(newWeekStart.getDate() - 7);
+        setWeekStart(newWeekStart);
+    }
+
+    const forward = () => {
+        const newWeekStart = new Date(weekStart);
+        newWeekStart.setDate(newWeekStart.getDate() + 7);
+        setWeekStart(newWeekStart);
+    }
+
+    const Week = () => {
+        let weekDate = new Date(weekStart);
+        const weekList = [];
+        for (let i = 0; i < 7; i++){
+            weekList.push(new Date (weekDate));
+            console.log(weekDate);
+            weekDate.setDate(weekDate.getDate() + 1);
+        }
+        weekDate = new Date(weekStart);
+        return (
+            <>
+            <button type="button" onClick={back}>Last Week</button>
+            <h3>{months[weekDate.getMonth()]} {weekDate.getFullYear()}</h3>
+            <button type="button" onClick={forward}>Next Week</button>
+                <div class="w-4/5 flex flex-row">
+                    {weekList.map(day => (
+                        <Day d={daysOfWeek[day.getDay()]} date={day.getDate()} />
+                    ))}
+                </div>
+            </>
         )
     }
 
@@ -46,15 +87,7 @@ function Dashboard () {
         <>
             <h2>Projects</h2>
             <div class="flex w-full p-10">
-                <div class="w-4/5 flex flex-row">
-                    <Day d="M" date='11/12' />
-                    <Day d="T" date='12/12' />
-                    <Day d="W" date='13/12' />
-                    <Day d="T" date='14/12' />
-                    <Day d="F" date='15/12' />
-                    <Day d="S" date='16/12' />
-                    <Day d="S" date='17/12' />
-                </div>
+                <Week />
                 <aside>
                     <section class="flex flex-row">
                         <button type="button" class="my-5 h-10 bg-emerald-400 hover:bg-emerald-500 active:bg-emerald-600 rounded-md text-white w-1/2 m-auto">New Project</button>
